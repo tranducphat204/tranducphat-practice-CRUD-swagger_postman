fdeb6a39c32854025f15b745ba982c61cdaac61e
phatdeptraiii <vitphat350@gmail.com>
2024-04-09 10:15:00 +0700

    ex2

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
+/**
+
+ * @OA\Get(
+ *     path="/api/users",
+ *     summary="Get all users",
+ *     @OA\Response(response="200", description="Success"),
+ *     security={{"bearerAuth": {}}}
+ * )
+ *
+ * @OA\Put(
+ *     path="/api/users/{id}",
+ *     summary="Update a user",
+ *     @OA\Parameter(
+ *         name="id",
+ *         in="path",
+ *         description="ID of the user",
+ *         required=true,
+ *         @OA\Schema(
+ *             type="integer",
+ *             format="int64"
+ *         )
+ *     ),
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\JsonContent(
+ *             @OA\Property(property="title", type="string"),
+ *             @OA\Property(property="description", type="string")
+ *         )
+ *     ),
+ *     @OA\Response(response="200", description="Success"),
+ *     @OA\Response(response="404", description="user not found"),
+ *     security={{"bearerAuth": {}}}
+ * )
+ *
+ * @OA\Get(
+ *     path="/api/users/{id}",
+ *     summary="Get a user by ID",
+ *     @OA\Parameter(
+ *         name="id",
+ *         in="path",
+ *         description="ID of the user",
+ *         required=true,
+ *         @OA\Schema(
+ *             type="integer",
+ *             format="int64"
+ *         )
+ *     ),
+ *     @OA\Response(response="200", description="Success"),
+ *     @OA\Response(response="404", description="user not found"),
+ *     security={{"bearerAuth": {}}}
+ * )
+ *
+ * @OA\user(
+ *     path="/api/users",
+ *     summary="Create a new user",
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\JsonContent(
+ *             @OA\Property(property="title", type="string"),
+ *             @OA\Property(property="description", type="string")
+ *         )
+ *     ),
+ *     @OA\Response(response="201", description="user created"),
+ *     security={{"bearerAuth": {}}}
+ * )
+ *
+ * @OA\Delete(
+ *     path="/api/users/{id}",
+ *     summary="Delete a user by ID",
+ *     @OA\Parameter(
+ *         name="id",
+ *         in="path",
+ *         description="ID of the user",
+ *         required=true,
+ *         @OA\Schema(
+ *             type="integer",
+ *             format="int64"
+ *         )
+ *     ),
+ *     @OA\Response(response="200", description="user deleted"),
+ *     @OA\Response(response="404", description="user not found"),
+ *     security={{"bearerAuth": {}}}
+ * )
+ */
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $user = DB::table('users')->get();
+        return $user;
+    }
+
+    public function update(Request $request)
+    {
+        DB::table('users')
+            ->where('id', $request->id)
+            ->update([
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'password' => $request->input('password')
+
+            ]);
+        $user = User::find($request->id);
+
+        if (!$user) {
+            return response()->json(['message' => 'Không tìm thấy bài viết'], 404);
+        }
+        return response()->json($user);
+    }
+
+    public function show(Request $request)
+    {
+        $user = User::find($request->id);
+
+        if (!$user) {
+            return response()->json(['message' => 'Không tìm thấy bài viết'], 404);
+        }
+        return response()->json($user);
+    }
+
+    public function store(Request $request)
+    {
+        $user = User::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => $request['password'],
+
+        ]);
+
+        return response()->json($user, 201);
+    }
+
+    public function destroy(Request $request)
+    {
+        $id = $request->query('id');
+        DB::table('users')->where('id', $request->id)->delete();
+        return response()->json(['message' => 'Xóa thành công']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,11 +21,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/posts', [PostController::class, 'index'])->name('index');
-
 Route::post('/posts/{id}', [PostController::class, 'show']);
-
 Route::post('/posts', [PostController::class, 'store']);
-
 Route::delete('/posts/{id}', [PostController::class, 'destroy']);
-
 Route::put('/posts/{id}', [PostController::class, 'update']);
+
+Route::get('users', [UserController::class, 'index']);
+Route::get('users/{id}', [UserController::class, 'show']);
+Route::post('users', [UserController::class, 'store']);
+Route::put('users/{id}', [UserController::class, 'update']);
+Route::delete('users/{id}', [UserController::class, 'destroy']);
